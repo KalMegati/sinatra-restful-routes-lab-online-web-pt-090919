d8f089d43dd287a43add3c16ef45b70173e56d28
Srikalyan Megati <kalmegati@gmail.com>
2019-12-22 07:41:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,23 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  
+  get "/recipes/new" do
+    
+  end
+  
+  get "/recipes/:id" do
+    @recipe = Recipe.find_by(params)
+    erb :recipe
+  end
+  
+  get "/recipes/:id/edit" do
+    
+  end
+  
+  get "/recipes" do
+    @recipes = Recipe.all
+    erb :index
+  end
 
 end

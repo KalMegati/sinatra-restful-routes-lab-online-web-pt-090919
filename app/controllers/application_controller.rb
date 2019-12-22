9c23b99d2047c80b23ba3da58555357fcadb09c9
Srikalyan Megati <kalmegati@gmail.com>
2019-12-22 08:15:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
   end
   
   patch "/recipes/:id" do
-    params.delete("_method")
+    #params.delete("_method")
     @recipe = Recipe.update(params)
     redirect to "/recipes/:id"
   end
